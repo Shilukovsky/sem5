de7f91e3ecbefa9cfa7da554a84b143695096b07
Nikolay <cccp_ussr@bk.ru>
2022-08-09 06:52:53 +0300

    added DZ36

---
+++ DZ36/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] CreateArrayRndInt (int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);    
+    }
+    return arr;
+}
+
+int GetSumNech (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 > 0)
+        {
+            sum = sum + array[i];
+        }    
+    }
+    return sum;
+  
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i == 0) Console.Write("[");
+        if(i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i] + "]");
+    }
+}
+
+int[] array = CreateArrayRndInt(4, -100, 100);
+PrintArray(array);
+int res = GetSumNech(array);
+Console.WriteLine(" -> ");
+Console.WriteLine(res);
